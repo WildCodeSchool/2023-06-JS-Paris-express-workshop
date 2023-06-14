9106b6e7ec71e622b771ddf4a5203b55cd7d4c0c
brian boudrioux <brian.boudrioux@wildcodeschool.com>
2023-06-14 11:05:45 +0200

    correction atelier

--- src/routes/albums/albumsController.js
+++ src/routes/albums/albumsController.js
@@ -1,28 +1,57 @@
-const connexion = require('../../../db-config');
-const db = connexion.promise();
+const {findAll, findById, findTracksByAlbumId, create, updateOne, deleteOne} = require("./albumsModel");
 
 const getAll = (req, res) => {
-  res.status(200).send('Get All route is OK');
+  findAll().then((albums) => res.status(200).json(albums))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json("error server");
+    }); 
 };
 
 const getOne = (req, res) => {
-  res.status(200).send('Get One route is OK');
+  findById(req.params.id).then(([album]) => {
+      if(!album) return res.sendStatus(404);
+      res.status(200).json(album);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json("error server");
+    });
 };
 
 const getTracksByAlbumId = (req, res) => {
-  res.status(200).send('Get Albums route is OK');
+  findTracksByAlbumId(req.params.id).then((tracks) => res.status(200).json(tracks))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json("error server");
+    });
 };
 
 const postAlbums = (req, res) => {
-  res.status(200).send('Post route is OK');
+  create(req.body).then((result) => res.status(201).json({id: result.insertId, ...req.body}))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json("error server");
+    });
 };
 
 const updateAlbums = (req, res) => {
-  res.status(200).send('Update route is OK');
+  updateOne(req.params.id, req.body).then(() => res.sendStatus(204))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json("error server");
+    });
 };
 
 const deleteAlbums = (req, res) => {
-  res.status(200).send('Delete route is Ok');
+  findById(req.params.id).then(([album]) => {
+    if (!album) return res.sendStatus(404);
+    deleteOne(req.params.id).then(() => res.sendStatus(204))
+      .catch((err) => {
+        console.error(err);
+        res.status(500).json("error server");
+      });
+  })
 };
 
 module.exports = {
